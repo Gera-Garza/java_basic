5d169f0d06ab66920ea321886d6f9a99825d92ac
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-25 16:27:01 -0500

    Lower y Upper camel case

--- src/NaminginJava.java
+++ src/NaminginJava.java
@@ -1,3 +1,4 @@
+        //Upper Camel Case
 public class NaminginJava {
     public static void main(String[] args) {
         int celphone = 333777;
@@ -15,6 +16,10 @@ public class NaminginJava {
         int MAX_WIDTH = 9999;
         int MIN_WIDTH = 1;
 
+        //Lower Camel Case
+        String fullName = "Gerardo Guillermo Garza Tamez";
+        int sizeInCentimeters = 26;
+        
 
     }
 }
