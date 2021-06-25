5c0f4e451f502e376d721983de2d993a95016e95
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-25 18:23:27 -0500

    creacion de arreglos

---
+++ src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        String androidVersions[] = new String[17];
+        String[] days = new String[7];
+
+        String cities[][] = new String[4][2];
+        int numbers[][][] = new int[1][2][3];
+        int numbers4[][][][] = new int[2][2][3][3];
+
+    }
+}
