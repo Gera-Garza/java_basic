c0d96d221d62ab15deef7474a05de8cb49b1c06b
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-25 16:20:13 -0500

    nombramiento de variables

---
+++ src/NaminginJava.java
@@ -0,0 +1,20 @@
+public class NaminginJava {
+    public static void main(String[] args) {
+        int celphone = 333777;
+        int celPhone = 555777;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Mexico";
+        String _backgroundColor = "green";
+
+        String currency_MX = "MXN";
+        String background$color = "blue";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+    }
+}
