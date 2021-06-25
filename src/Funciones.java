773ea18e58ca6e759cbdaf23fce6528528967461
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-25 17:41:37 -0500

    creacion de funciones en MathematicOperations

---
+++ src/Funciones.java
@@ -0,0 +1,43 @@
+public class Funciones {
+    public static void main(String[] args) {
+        double y=3;
+
+        //Area de un ciruclo
+        //pi * r2
+        System.out.println(area(y));
+
+        //Area de una esfera
+        //4*PI*r2
+        System.out.println(sphereArea(y));
+
+        //Volumen de una esfera
+        //(4/3)*PI*r3
+        System.out.println(sphereVol(y));
+
+        //convertidor de moneda a doalres
+        System.out.println("Pesos a dolares: " + covertToDolar(1000,"MXN"));
+    }
+
+    public static double area(double rad){
+        return Math.PI*Math.pow(rad,2);
+    }
+    public static double sphereArea(double rad){
+        return 4*Math.PI*Math.pow(rad,2);
+    }
+    public static double sphereVol(double rad){
+        return (4/3)*Math.PI*Math.pow(rad,3);
+    }
+
+    public static double covertToDolar(double quantity, String currency){
+        //MXN
+        switch (currency){
+            case "MXN":
+                quantity *= 0.052;
+                break;
+            case "COP":
+                quantity *= 0.00031;
+                break;
+        }
+        return  quantity;
+    }
+}
