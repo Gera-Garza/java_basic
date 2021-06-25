5821d9fc7bd800841cb354ee3ff7f62479e9bea9
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-06-25 18:02:51 -0500

    creacion de forloop

---
+++ src/ForLoop.java
@@ -0,0 +1,21 @@
+public class ForLoop {
+
+    static  boolean isTurnOnLigth = false;
+
+    public static void main(String[] args) {
+        turnOnOffLigth();
+
+        for (int i = 1; i <= 10; i++) {
+            printSOS();
+        }
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+
+    public static boolean turnOnOffLigth() {
+        isTurnOnLigth = (isTurnOnLigth)?false:true;
+        return isTurnOnLigth;
+    }
+}
